63aa304c4e11a8a52a8528c050b84929ef893635
Panagiotis Iatrou <iatrou.panag@gmail.com>
2018-02-18 19:49:21 +0200

    Add files via upload

---
+++ Examples/HierarchySystemExample0/HierarchySystemExample0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using HierarchySystem;
+
+namespace HierarchySystemExample0
+{
+    class Program
+	{
+		static void Main(string[] args)
+		{
+			HItem grandfather = new HItem();
+			HItem father = new HItem();
+			HItem daughter = new HItem();
+
+			father.SetParent(grandfather);
+			daughter.SetParent(father);
+
+			HItem _SameGrandfather = grandfather.GetChild(0).GetChild(0);
+
+			Console.WriteLine(_SameGrandfather.childCount);
+			Console.Read();
+		}
+	}
+}
